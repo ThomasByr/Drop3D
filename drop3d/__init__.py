17ed704ab9e6c6cf776ef4a0f5768175bbdbbb81
Thomas Byr <tbouyer2000@gmail.com>
2022-02-06 21:12:20 +0100

    init scope

--- drop3d/__init__.py
+++ drop3d/__init__.py
@@ -1,1 +1,11 @@
 from .imports import *
+
+# Hello from Drop3d!
+print(f"Hello from Drop3d - please visit us on GitHub")
+
+from .__version__ import __title__
+from .__version__ import __description__
+from .__version__ import __url__
+from .__version__ import __version__
+from .__version__ import __author__
+from .__version__ import __license__
