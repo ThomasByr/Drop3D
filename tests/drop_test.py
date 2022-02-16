8cf07571a6311c4029ef9ddbb84b8e8700574f73
Thomas Byr <tbouyer2000@gmail.com>
2022-02-16 16:04:59 +0100

    small tests to run with pytest

--- tests/drop_test.py
+++ tests/drop_test.py
@@ -3,7 +3,8 @@ import matplotlib.pyplot as plt
 from drop3d import *
 
 gen_mode(GenMode.FIXED)
-precision(20)
+mesh_mode(MeshMode.RANDOM)
+precision(30)
 squish(1)
 
 create_drop(x=0, y=0, z=0, min_r=9, max_r=11)
