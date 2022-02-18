32a559d1bf0fb7bf008610cd9c72c9c41c59e75a
Thomas Byr <tbouyer2000@gmail.com>
2022-02-18 12:43:08 +0100

    small tests to run with pytest

--- tests/drop_test.py
+++ tests/drop_test.py
@@ -2,18 +2,22 @@ import matplotlib.pyplot as plt
 
 from drop3d import *
 
-gen_mode(GenMode.FIXED)
-mesh_mode(MeshMode.RANDOM)
-precision(30)
+scene(-100, 100, -100, 100, -100, 100)
+print(scene())
+gen_mode(GenMode.RANDOM)
+mesh_mode(MeshMode.UNIFORM)
+precision(20)
 squish(1)
 
-create_drop(x=0, y=0, z=0, min_r=9, max_r=11)
+for _ in range(100):
+    create_drop(min_r=9, max_r=11)
 
-plt.close()
+plt.close("all")
 plt.clf()
 plt.cla()
+
 fig = plt.figure(0)
-ax = fig.add_subplot(111, projection="3d")
+ax = fig.add_subplot(111, projection="3d", aspect="auto")
 
 for drop in each_drop():
     x, y, z = as_surface(drop)
